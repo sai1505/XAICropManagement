0b9879d29e7d6f2ac9486b201e01b86e92009a2c
sai1505 <vsai1505.bona@gmail.com>
2025-12-09 23:29:03 +0530

    Redesigned Landing Page.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,7 @@
-import Footer from "./components/Footer";
-import HomePage from "./components/HomePage";
 import NavBar from "./components/Navbar";
 
 export default function App() {
   return (
-    <>
-      <NavBar />
-      <HomePage />
-      <Footer />
-    </>
+    <NavBar />
   );
 }
