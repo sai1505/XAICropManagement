45d229d04369dcef514ebcd127618b4afc9e075c
sai1505 <vsai1505.bona@gmail.com>
2025-12-09 23:33:37 +0530

    Redesignd Landign Page.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
 import NavBar from "./components/Navbar";
+import HomePage from "./components/HomePage";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <NavBar />
+    <>
+      <NavBar />
+      <HomePage />
+      <Footer />
+    </>
+
   );
 }
