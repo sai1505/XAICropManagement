0a9f6f6f1fd18c366b425c1bf56e43c49cbbe39c
sai1505 <vsai1505.bona@gmail.com>
2025-12-14 16:51:16 +0530

    Successfullt implemented Sign Up work flow.

--- client/src/components/Auth/SignUpPage.jsx
+++ client/src/components/Auth/SignUpPage.jsx
@@ -1,11 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
+import { supabase } from "../../supabase/SupabaseClient";
 
 export default function SignUpPage() {
     const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [displayName, setDisplayName] = useState("");
+
+
+    const checks = {
+        length: password.length >= 6,
+        upper: /[A-Z]/.test(password),
+        lower: /[a-z]/.test(password),
+        number: /[0-9]/.test(password),
+        special: /[^A-Za-z0-9]/.test(password),
+    };
+
+    const isValidPassword = Object.values(checks).every(Boolean);
+
+    function PasswordRule({ ok, text }) {
+        return (
+            <div className={`flex items-center gap-2 ${ok ? "text-lime-600" : "text-gray-400"}`}>
+                <span
+                    className={`w-3.5 h-3.5 flex items-center justify-center rounded-full border
+          ${ok
+                            ? "bg-lime-500 border-lime-500"
+                            : "border-gray-400"
+                        }`}
+                />
+                <span className="text-xs">{text}</span>
+            </div>
+        );
+    }
+
+    const handleSignUp = async () => {
+        const { data, error } = await supabase.auth.signUp({
+            email,
+            password,
+            options: {
+                emailRedirectTo: "http://localhost:5173/signin",
+                data: {
+                    display_name: displayName,
+                }
+            }
+        });
+
+        if (error) {
+            toast.error(error.message);
+            return;
+        }
+
+        toast.success("Check your email for verification");
+    }
 
     return (
         <div className="min-h-screen flex">
+            <Toaster position="bottom-right"
+                toastOptions={{
+                    style: {
+                        fontFamily: "Poppins, sans-serif",
+                    },
+                }} />
             {/* LEFT – IMAGE */}
             <div className="w-1/2 bg-white flex items-center justify-center">
                 <img
@@ -25,22 +82,39 @@ export default function SignUpPage() {
                         <input
                             type="text"
                             placeholder="Full name"
+                            value={displayName}
+                            onChange={(e) => setDisplayName(e.target.value)}
                             className="w-full border font-poppins border-lime-400 rounded-3xl px-4 py-3 focus:bg-lime-200 focus:border-white focus:outline-none"
                         />
 
                         <input
                             type="email"
                             placeholder="Email address"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
                             className="w-full border font-poppins border-lime-400 rounded-3xl px-4 py-3 focus:bg-lime-200 focus:border-white focus:outline-none"
                         />
 
                         <input
                             type="password"
                             placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
                             className="w-full border font-poppins border-lime-400 rounded-3xl px-4 py-3 focus:bg-lime-200 focus:border-white focus:outline-none"
                         />
 
-                        <button className="w-full text-neutral-950 font-poppins bg-lime-200 py-3 rounded-3xl hover:bg-lime-400 transition">
+                        <div className="text-xs font-poppins space-y-1">
+                            <PasswordRule ok={checks.length} text="Password must be at least 6 characters" />
+                            <PasswordRule ok={checks.upper} text="Password must contain 1 uppercase letter" />
+                            <PasswordRule ok={checks.lower} text="Password must contain 1 lowercase letter" />
+                            <PasswordRule ok={checks.number} text="Password must contain 1 number" />
+                            <PasswordRule ok={checks.special} text="Password must contain 1 special character" />
+                        </div>
+
+
+                        <button className="w-full text-neutral-950 font-poppins bg-lime-200 py-3 rounded-3xl hover:bg-lime-400 transition"
+                            disabled={!isValidPassword}
+                            onClick={handleSignUp}>
                             Sign up
                         </button>
 
